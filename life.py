92402dbd44fb2ae38521e1c224770b4d997a4b19
Scott Welsh <throwitinthecoffee@gmail.com>
2018-08-11 09:15:17 -0700

    Functions for creating a fully dead state and a randomized state added.

--- life.py
+++ life.py
@@ -1,4 +1,10 @@
+import random
+
 print("running")
+
+HEIGHT = 5
+WIDTH = 5
+RANDOM_THRESHOLD = 0.5
 
 def create_dead_state (height, width):
 	print("height is " + str(height))
@@ -6,7 +12,23 @@ def create_dead_state (height, width):
 	arr = [[0] * height for i in range(width)]
 	return arr
 
+def create_random_state (height, width):
+	state = create_dead_state(height, width)
 
-r = create_dead_state(5, 5)
+	# TODO: randomize each element of 'state' to 0 or 1
 
-print(r)
+	for x in range(height):
+		for y in range(width):
+			random_number = random.random()
+			if random_number >= RANDOM_THRESHOLD:
+				state[x][y] = 1
+
+	return state
+
+def render (state):
+	print(state)
+
+
+r = create_random_state(HEIGHT, WIDTH)
+
+render(r)
