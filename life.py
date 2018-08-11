a9389a5990d1fc0579022405805411c262241d08
Scott Welsh <throwitinthecoffee@gmail.com>
2018-08-11 09:46:45 -0700

    Fixed width / height bug and finished render function.
    A bug was crossing references to width and height and would crash if they were different numbers.
    
    Render function shows the grid with '#' for 1 and a space for 0. Clear lambda added for refreshing the terminal, but is commented out of the render function for now since the game doesn't actually need to run continuously yet.

--- life.py
+++ life.py
@@ -1,34 +1,48 @@
-import random
+import random, os
+
+clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 
 print("running")
 
-HEIGHT = 5
-WIDTH = 5
+HEIGHT = 20
+WIDTH = 40
 RANDOM_THRESHOLD = 0.5
 
-def create_dead_state (height, width):
+def dead_state (height, width):
 	print("height is " + str(height))
 	print("width is " + str(width))
-	arr = [[0] * height for i in range(width)]
+	arr = [[0] * width for i in range(height)]
 	return arr
 
-def create_random_state (height, width):
-	state = create_dead_state(height, width)
-
-	# TODO: randomize each element of 'state' to 0 or 1
+def random_state (height, width):
+	state = dead_state(height, width)
 
 	for x in range(height):
 		for y in range(width):
 			random_number = random.random()
-			if random_number >= RANDOM_THRESHOLD:
+			if random_number <= RANDOM_THRESHOLD:
 				state[x][y] = 1
 
 	return state
 
+def print_line(line):
+	string = "|"
+	for i in range(len(line)):
+		if line[i] == 0:
+			string += " "
+		if line[i] == 1:
+			string += "#"
+	string += "|"
+	print(string)
+
 def render (state):
-	print(state)
+	# clear()
+	print("-" * (WIDTH + 2))
+	for i in range(len(state)):
+		print_line(state[i])
+	print("-" * (WIDTH + 2))
 
 
-r = create_random_state(HEIGHT, WIDTH)
+r = random_state(HEIGHT, WIDTH)
 
 render(r)
